13e4b09269bedbddb39d527a3a275132356b6a8d
nazmulbhuyian <nazmulbhuyian000@gmail.com>
2023-09-14 15:16:04 +0600

    place add api add

--- models/AllPlaces.js
+++ models/AllPlaces.js
@@ -2,33 +2,63 @@ const mongoose = require("mongoose");
 const allPlacesSchema = new mongoose.Schema({
     place_name: {
         type: String,
-        required: [true, "Hotel Name must be required"],
+        required: [true, "Place Name must be required"],
         minLength: [3, "At least 3 caracted must be provide"],
         maxLength: [40, "Name is to large"],
     },
     location_name: {
         type: String,
-        required: [true, "Hotel Name must be required"],
+        required: [true, "Location Name must be required"],
         minLength: [3, "At least 3 caracted must be provide"],
         maxLength: [40, "Name is to large"],
     },
-    img: {
-        required: true,
-        type: String
-    },
-    over: [
+    overview: [
         {
             image: String,
         },
     ],
     tradition: [
         {
-            name: String,
+            title: String,
             image: String,
-            des: String,
+            about: String,
         },
     ],
-
+    three_d_maps: {
+        type: String
+    },
+    thinks_to_do: [
+        {
+            title: String,
+            image: String,
+            about: String,
+        },
+    ],
+    vr_image: [
+        {
+            image: String,
+        },
+    ],
+    discover_360_image: [
+        {
+            image: String,
+        },
+    ],
+    about: {
+        type: String
+    },
+    history: {
+        type: String
+    },
+    wildlife: {
+        type: String
+    },
+    warning: {
+        type: String
+    },
+    trial_guide: {
+        type: String
+    },
 })
 
 const AllPlaces = mongoose.model("allplaces", allPlacesSchema);
