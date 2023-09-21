ea196008d0b8ec0d5363d4483215a0c61667e4bd
nazmulbhuyian <nazmulbhuyian000@gmail.com>
2023-09-21 22:59:26 +0600

    new change

--- routes/allTouristSpotNameRoute.js
+++ routes/allTouristSpotNameRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const allTouristSpotNameControllers=require('../controllers/allTouristSpotNameControllers');
 
 router.route('/').post(allTouristSpotNameControllers.postAllTouristSpot).get(allTouristSpotNameControllers.getAllTouristSpot)
-router.route('/hotelAddPlace').get(allTouristSpotNameControllers.getAllTouristSpotForBusinessHotelAddPlaceName)
-router.route('/hotelNearSpot').get(allTouristSpotNameControllers.getAllHotelNearSpotForBusinessHotelAdd)
+router.route('/hotelAddPlace').post(allTouristSpotNameControllers.getAllTouristSpotForBusinessHotelAddPlaceName)
+router.route('/hotelNearSpot').post(allTouristSpotNameControllers.getAllHotelNearSpotForBusinessHotelAdd)
 
 
 module.exports=router;
